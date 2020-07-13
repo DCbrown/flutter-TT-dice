feb40c0826e0d460172aaa249c2fe98fefa38ef6
donovanbrown <browncdonovan@gmail.com>
2020-07-13 13:01:18 -0400

    Refactor project move images outside of it's directory add logic and case statements to show dice image values

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 
 void main() => runApp(MaterialApp(
   home: HomePage(),
 ));
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePage createState() => _HomePage();
 }
@@ -19,8 +16,9 @@ class HomePage extends StatefulWidget {
 
     int totalRoll = 0;
     int saveTotalDiceRoll = 0;
-    int leftDiceNumber = 1;
+    int diceNumber = 1;
     int result = 2;
+    String diceDir = "d4";
     String diceLabel = "D6: Six sided dice";
 
     _diceSelectionMenu(BuildContext context) {
@@ -32,108 +30,6 @@ class HomePage extends StatefulWidget {
       });
     }
 
-    _buildChild(BuildContext context) => Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /*...*/
-            },
-            child: Text(
-              "D4: Four-sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              diceLabel = "D6: Six sided dice";
-              Navigator.of(context, rootNavigator: true).pop(result);
-            },
-            child: Text(
-              "D6: Six sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /*...*/
-            },
-            child: Text(
-              "D8: Eight sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /*...*/
-            },
-            child: Text(
-              "D10: 10 sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /*...*/
-            },
-            child: Text(
-              "D12: 12 sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          FlatButton(
-            color: Colors.blue,
-            textColor: Colors.white,
-            disabledColor: Colors.grey,
-            disabledTextColor: Colors.black,
-            padding: EdgeInsets.all(8.0),
-            splashColor: Colors.blueAccent,
-            onPressed: () {
-              /*...*/
-            },
-            child: Text(
-              "D20: 20 sided dice",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          )
-        ],
-      ),
-    );
-
-
 
 
     @override
@@ -142,15 +38,66 @@ class HomePage extends StatefulWidget {
       Timer.run(() {
         _diceSelectionMenu(context);
       });
-
     }
 
     void diceRoll() {
-      leftDiceNumber = Random().nextInt(6) + 1;
+      // diceNumber = Random().nextInt(6) + 1;
+      var dice = diceLabel;
+      print('one');
+      switch(dice) {
+        case "D4: Four sided dice": {
+          print('4');
+          diceDir = "d4";
+          diceNumber = Random().nextInt(4) + 1;
+        }
+        break;
+
+        case "D6: Six sided dice": {
+          print('6');
+          diceDir = "d6";
+          diceNumber = Random().nextInt(6) + 1;
+        }
+        break;
+
+        case "D8: Eight sided dice": {
+          print('8');
+          diceDir = "d8";
+          diceNumber = Random().nextInt(8) + 1;
+        }
+        break;
+
+        case "D10: Ten sided dice": {
+          print('10');
+          diceDir = "d10";
+          diceNumber = Random().nextInt(10) + 1;
+        }
+        break;
+
+        case "D12: Twelve sided dice": {
+          print('12');
+          diceDir = "d12";
+          diceNumber = Random().nextInt(12) + 1;
+        }
+        break;
+
+        case "D20: Twenty sided dice": {
+          print('20');
+          diceDir = "d20";
+          diceNumber = Random().nextInt(20) + 1;
+        }
+        break;
+
+        default: {
+          print("Invalid choice");
+        }
+        break;
+
+      }
+
     }
 
     void saveDiceRoll() {
-      totalRoll = leftDiceNumber + totalRoll;
+      totalRoll = diceNumber + totalRoll;
       saveTotalDiceRoll = totalRoll;
     }
 
@@ -170,6 +117,129 @@ class HomePage extends StatefulWidget {
         saveTotalDiceRoll--;
       }
     }
+
+    _buildChild(BuildContext context) => Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D4: Four sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D4: Four-sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D6: Six sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D6: Six sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D8: Eight sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D8: Eight sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D10: Ten sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D10: 10 sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D12: Twelve sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D12: 12 sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+          FlatButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            disabledColor: Colors.grey,
+            disabledTextColor: Colors.black,
+            padding: EdgeInsets.all(8.0),
+            splashColor: Colors.blueAccent,
+            onPressed: () {
+              setState(() {
+                diceLabel = "D20: Twenty sided dice";
+                diceRoll();
+                Navigator.of(context, rootNavigator: true).pop(result);
+              });
+            },
+            child: Text(
+              "D20: 20 sided dice",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ],
+      ),
+    );
 
     Widget build(BuildContext context) {
       SystemChrome.setPreferredOrientations([
@@ -195,7 +265,6 @@ class HomePage extends StatefulWidget {
                         "Save Value: $saveTotalDiceRoll",
                         style: TextStyle(fontSize: 30, color: Colors.white),
                         textAlign: TextAlign.center,
-
                       ),
                     ),
                   ],
@@ -218,15 +287,14 @@ class HomePage extends StatefulWidget {
                           ),
                         ),
                       ),
-
-                    )
+                    ),
                   ],
                 ),
                 Row(
                   children: <Widget>[
                     Expanded(
                       child: Padding(
-                        child: Image.asset('Images/d6/$leftDiceNumber.png'),
+                        child: Image.asset('images/$diceDir$diceNumber.png'),
                         padding: EdgeInsets.all(60),
                       ),
                     ),
@@ -250,7 +318,6 @@ class HomePage extends StatefulWidget {
                           style: TextStyle(fontSize: 20)
                       ),
                     ),
-
                     new RaisedButton(
                       onPressed: () {
                         setState(() {
@@ -267,52 +334,55 @@ class HomePage extends StatefulWidget {
                     ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    new RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          saveDiceRoll();
-                        });
-                      },
-                      textColor: Colors.white,
-                      color: Color(0xFF27ae60),
-                      padding: const EdgeInsets.all(8.0),
-                      child: new Text(
-                          "Save",
-                          style: TextStyle(fontSize: 20)
+                Padding(
+                  padding: const EdgeInsets.only(top:10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            saveDiceRoll();
+                          });
+                        },
+                        textColor: Colors.white,
+                        color: Color(0xFF27ae60),
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Text(
+                            "Save",
+                            style: TextStyle(fontSize: 20)
+                        ),
                       ),
-                    ),
-                    new RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          diceRoll();
-                        });
-                      },
-                      textColor: Colors.white,
-                      color: Color(0xFF27ae60),
-                      padding: const EdgeInsets.all(8.0),
-                      child: new Text(
-                          "Roll",
-                          style: TextStyle(fontSize: 20)
+                      new RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            diceRoll();
+                          });
+                        },
+                        textColor: Colors.white,
+                        color: Color(0xFF27ae60),
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Text(
+                            "Roll",
+                            style: TextStyle(fontSize: 20)
+                        ),
                       ),
-                    ),
-                    new RaisedButton(
-                      onPressed: () {
-                        setState(() {
-                          clearDiceRoll();
-                        });
-                      },
-                      textColor: Colors.white,
-                      color: Color(0xFF27ae60),
-                      padding: const EdgeInsets.all(8.0),
-                      child: new Text(
-                          "Clear",
-                          style: TextStyle(fontSize: 20)
+                      new RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            clearDiceRoll();
+                          });
+                        },
+                        textColor: Colors.white,
+                        color: Color(0xFF27ae60),
+                        padding: const EdgeInsets.all(8.0),
+                        child: new Text(
+                            "Clear (Hold for 3 sec)",
+                            style: TextStyle(fontSize: 20)
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
